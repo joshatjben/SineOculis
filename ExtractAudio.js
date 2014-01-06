20154d0b38a9b3fff7f05c824835117c26b0467d
Josh Bennett <josh@j-ben.com>
2014-01-06 15:03:38 -0500

    first commit

---
+++ ExtractAudio.js
@@ -0,0 +1,152 @@
+#!/usr/bin/env node
+
+var async = require('async'),
+  argv = require('optimist')
+    .string('inputDir')
+    .demand('inputDir')
+    .string('outputDir')
+    .demand('outputDir')
+    .string('fileType')
+    .demand('fileType')
+    .argv,
+  exec = require('child_process').exec,
+  filesToConvert = [],
+  fs = require('fs'),
+  isWin = process.platform.match(/^win/),
+  SLASH = isWin ? "\\" : "/",
+  ENDS_WITH_SLASH = new RegExp( "(?:\\|/)$", "gi"),
+  MATCH_FILE = new RegExp( "\." +argv.fileType + "$", "gi" ),
+  ECHO = true;
+
+// Make sure paths always end with slash
+argv.inputDir = ENDS_WITH_SLASH.test(argv.inputDir) ? argv.inputDir : argv.inputDir + SLASH;
+argv.outputDir = ENDS_WITH_SLASH.test(argv.outputDir) ? argv.inputDir : argv.outputDir + SLASH;
+
+// Execute at launch
+function main() {
+
+  async.series([
+      function (callback) {
+        walk(argv.inputDir, function (err, results) {
+          filesToConvert = results;
+          callback(err, results);
+        });
+      },
+      function (callback) {
+        async.each(
+          filesToConvert,
+          convertFile,
+          function (err) {
+            //callback(err, null);
+          }
+        );
+      }
+    ],
+
+    function (err, results) {
+      if (err) console.log(err);
+      
+      console.log(filesToConvert.length.toString() + " files extracted.")
+    }
+  );
+}
+
+// Run the fffmpeg command against all the files    
+function convertFile(item, callback) {
+  if(!MATCH_FILE.test(item)){
+    callback(null);
+    return;
+  }
+  var command = 'ffmpeg.exe -i "' + argv.inputDir + item + '" -vn -y -acodec copy "' + argv.outputDir + item + '"';
+  async.series([
+      function (cb) {
+        execute(command, cb);
+        
+      }
+    ],
+    function(err){
+      callback(err, null);
+    }
+  )
+}
+
+// Print execution info to the console
+function printExecResultsToConsole(error, stdout, stderr, command) {
+  console.log("--------");
+  console.log("Command");
+  console.log("--------");
+  console.log(command);
+  console.log("");
+
+  console.log("");
+  console.log("Restuls");
+  console.log("--------");
+
+  if (stderr !== "" && typeof stderr !== 'undefined' && stderr === null) {
+    console.log(stderr);
+    console.log("");
+    console.log("");
+  }
+
+  if (error !== null) {
+    console.log(error);
+    console.log("");
+    console.log("");
+  }
+
+  console.log(stdout);
+  console.log("");
+  console.log("");
+
+}
+
+function execute(command, callback) {
+  exec(command, function (error, stdout, stderr) {
+    handleExecDone(error, stdout, stderr, callback, command);
+  });
+}
+
+// Handle regular command executions
+function handleExecDone(error, stdout, stderr, callback, command) {
+  if(ECHO){
+    printExecResultsToConsole(error, stdout, stderr, command);
+  }
+
+  if (error !== null) {
+    callback(stderr, null);
+    return;
+  }
+
+  if (stderr !== "" && typeof stderr !== 'undefined' && stderr === null) {
+    callback(stderr, null);
+    return;
+  }
+
+  callback(null, stdout);
+}
+
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      //file = dir + '/' + file;
+      file = file;
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file);
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+};
+
+main();
