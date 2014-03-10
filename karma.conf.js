92ba563ff6e2c040c5270b91d682c9f65c29bab7
unknown <jbennett@hirtlecallaghan.com>
2014-03-10 14:16:41 -0400

    parseFromString now working with a default parser

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/underscore/underscore.js',
       'lib/*.js',
       'test/**/*.js'
     ],
--- test/TimeParser.spec.js
+++ test/TimeParser.spec.js
@@ -1,43 +1,39 @@
 describe("TimeParser", function () {
   it("returns an array of objects containing time,songtitle, songnumber using standard [n. title here hh:mm] format", function () {
-    var inputTimeString = 
-        "1. Test title here 0:00\n\r" +
-        "2. The second track 03:31\n\r" +
-        "3. Third track 07:09\n\r";
-    
-    var expectedParsedOutput =
-      [
-        {
-          "TrackNumber": 1,
-          "SongTitle": "Test title here",
-          "startingAt": {
-            "hour": 0,
-            "minute": 0,
-            "second": 0
-          }
-        },
-        {
-          "TrackNumber": 2,
-          "SongTitle": "The second track",
-          "startingAt": {
-            "hour": 0,
-            "minute": 3,
-            "second": 31
-          }
-        },
-        {
-          "TrackNumber": 3,
-          "SongTitle": "Third track",
-          "startingAt": {
-            "hour": 0,
-            "minute": 7,
-            "second": 9
-          }
-        }
-      ];
-    
-    var actualParsedOutput = window.me.joshbennett.TimeParser.parseFromString(inputTimeString);
-    
+
+    var inputTimeString =
+      "1. Test title here 0:00\n\r" +
+      "2. The second track 03:31\n\r" +
+      "3. Third track 07:09\n\r";
+
+    var expectedParsedOutput = [{
+      "TrackNumber": 1,
+      "SongTitle": "Test title here",
+      "startingAt": {
+        "hour": 0,
+        "minute": 0,
+        "second": 0
+      }
+    }, {
+      "TrackNumber": 2,
+      "SongTitle": "The second track",
+      "startingAt": {
+        "hour": 0,
+        "minute": 3,
+        "second": 31
+      }
+    }, {
+      "TrackNumber": 3,
+      "SongTitle": "Third track",
+      "startingAt": {
+        "hour": 0,
+        "minute": 7,
+        "second": 9
+      }
+    }];
+
+    var actualParsedOutput = new window.me.joshbennett.TimeParser.parseFromString(inputTimeString);
+
     expect(actualParsedOutput).toEqual(expectedParsedOutput);
   });
 });
